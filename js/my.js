1bf01a73822c7b4f54f0419af7e498f2ab60f98b
Katarzyna Tabara <katarzynatabara35@gmail.com>
2018-06-14 00:09:29 +0200

    Start page

---
+++ js/my.js
@@ -0,0 +1,11 @@
+var div = $(".logo_start");
+div.animate({height: '100px', opacity: '0.2'}, "speed");
+div.animate({height: '-100px', opacity: '0.3'}, "speed");
+div.animate({opacity: '0.4'}, "speed");
+div.animate({opacity: '0.5'}, "speed");
+div.animate({opacity: '0.6'}, "speed");
+div.animate({opacity: '0.7'}, "speed");
+div.animate({opacity: '0.8'}, "speed");
+div.animate({opacity: '0.9'}, "speed");
+div.animate({opacity: '1'}, "slow");
+
